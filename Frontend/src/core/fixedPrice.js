992f3d9c246aecd6c60c5028b5161dbc978018db
sarin punk <sarin.bhattarai@icp.edu.np>
2022-05-24 10:33:39 +0545

    Correction on minor mistake

--- Frontend/src/core/fixedPrice.js
+++ Frontend/src/core/fixedPrice.js
@@ -7,31 +7,31 @@ export const prices = [
 
   {
     _id: 1,
-    name: "Rs 0 to Rs 900",
-    array: [0, 900],
+    name: "Rs 0 to Rs 30",
+    array: [0, 30],
   },
 
   {
     _id: 2,
-    name: "Rs 1000 to Rs 1900",
-    array: [1000, 1900],
+    name: "Rs 30 to Rs 60",
+    array: [30, 60],
   },
 
   {
     _id: 3,
-    name: "Rs 2000 to Rs 2900",
-    array: [2000, 2900],
+    name: "Rs 60 to Rs 100",
+    array: [60, 100],
   },
 
   {
     _id: 4,
-    name: "Rs 3000 to Rs 3900",
-    array: [3000, 3900],
+    name: "Rs 100 to Rs 150",
+    array: [100, 150],
   },
 
   {
     _id: 5,
-    name: "More than Rs 4000",
-    array: [4000, 20000],
+    name: "More or exactly Rs 200",
+    array: [200, 20000],
   },
 ];
