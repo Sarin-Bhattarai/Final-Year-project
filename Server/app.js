7e780cf840494fba87b6b9149b09a136539e6bfd
sarin punk <sarin.bhattarai@icp.edu.np>
2022-06-07 10:30:11 +0545

    cors

--- Server/app.js
+++ Server/app.js
@@ -18,7 +18,7 @@ const orderRoutes = require("./routes/order");
 
 //app
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use("/uploads", express.static("uploads"));
 
 //db
